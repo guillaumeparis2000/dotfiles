b84c827c11efb0f50ef5d55d8d9cacf7b75c485c
Guillaume PARIS <guillaume.paris@letsbonus.com>
2014-11-21 13:01:25 +0100

    Add osx default configuration script

---
+++ osx/configure.sh
@@ -0,0 +1,149 @@
+#!/usr/bin/env bash
+
+##############
+# General UI
+##############
+
+# Menu bar: show remaining battery time (on pre-10.8); hide percentage
+# defaults write com.apple.menuextra.battery ShowTime -string "YES"
+
+# Enable subpixel font rendering on non-Apple LCDs
+defaults write NSGlobalDomain AppleFontSmoothing -int 2
+
+# Reveal IP address, hostname, OS version, etc. when clicking the clock in the login window
+sudo defaults write /Library/Preferences/com.apple.loginwindow AdminHostInfo HostName
+
+# Restart automatically if the computer freezes
+sudo systemsetup -setrestartfreeze On
+
+# Enable remote SSH login
+sudo systemsetup -setremotelogin On
+
+# Check for software updates daily, not just once per week
+defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
+
+################
+# Finder
+################
+
+# Show Library folder
+chflags nohidden ~/Library
+
+# Show all filename extensions
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+# Allow text selection in Quick Look
+defaults write com.apple.finder QLEnableTextSelection -bool true
+
+# Disable the warning when changing a file extension
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+
+# Expand save panel by default
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+
+# Expand print panel by default
+defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
+
+# Display full POSIX path as Finder window title
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+
+# Avoid creating .DS_Store files on network volumes
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+
+# Save to disk (not to iCloud) by default
+defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
+
+# Automatically quit printer app once the print jobs complete
+defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
+
+# Default to list view in Finder windows (others: `icnv`, `clmv`, `Flwv`)
+#defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
+
+############
+# Keyboard
+############
+
+# Enable full keyboard access for all controls (e.g. enable Tab in modal dialogs)
+defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
+
+# Automatically illuminate built-in MacBook keyboard in low light
+defaults write com.apple.BezelServices kDim -bool true
+
+# Turn off keyboard illumination when computer is not used for 5 minutes
+defaults write com.apple.BezelServices kDimTime -int 300
+
+# Enable screen zoom with ctrl+scroll
+defaults write com.apple.universalaccess closeViewScrollWheelToggle -bool true
+defaults write com.apple.universalaccess HIDScrollZoomModifierMask -int 262144
+defaults write com.apple.universalaccess closeViewPanningMode -int 2
+
+##################
+# Spotlight
+##################
+
+# Disable Spotlight indexing for any volume that gets mounted and has not yet
+# been indexed before.
+sudo defaults write /.Spotlight-V100/VolumeConfiguration Exclusions -array "/Volumes"
+
+# Change indexing order and disable some file types
+# defaults write com.apple.spotlight orderedItems -array \
+#   '{"enabled" = 1;"name" = "APPLICATIONS";}' \
+#   '{"enabled" = 1;"name" = "SYSTEM_PREFS";}' \
+#   '{"enabled" = 1;"name" = "DIRECTORIES";}' \
+#   '{"enabled" = 0;"name" = "PDF";}' \
+#   '{"enabled" = 0;"name" = "FONTS";}' \
+#   '{"enabled" = 0;"name" = "DOCUMENTS";}' \
+#   '{"enabled" = 0;"name" = "MESSAGES";}' \
+#   '{"enabled" = 0;"name" = "CONTACT";}' \
+#   '{"enabled" = 0;"name" = "EVENT_TODO";}' \
+#   '{"enabled" = 0;"name" = "IMAGES";}' \
+#   '{"enabled" = 0;"name" = "BOOKMARKS";}' \
+#   '{"enabled" = 0;"name" = "MUSIC";}' \
+#   '{"enabled" = 0;"name" = "MOVIES";}' \
+#   '{"enabled" = 0;"name" = "PRESENTATIONS";}' \
+#   '{"enabled" = 0;"name" = "SPREADSHEETS";}' \
+#   '{"enabled" = 0;"name" = "SOURCE";}'
+
+# Load new settings before rebuilding the index
+# killall mds
+# Make sure indexing is enabled for the main volume
+# sudo mdutil -i on /
+# Rebuild the index from scratch
+# sudo mdutil -E /
+
+##################
+# Time Machine
+##################
+
+# Prevent Time Machine from prompting to use new hard drives as backup volume
+defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
+
+##################
+# Safari
+##################
+
+defaults write com.apple.Safari IncludeDebugMenu 1
+
+##################
+# iTunes
+##################
+
+# Disable all the Ping stuff in iTunes
+defaults write com.apple.iTunes disablePingSidebar -bool true
+defaults write com.apple.iTunes disablePing -bool true
+
+##################
+# Folders
+##################
+
+# Not share public folder and remove it
+sudo sharing -r $HOME/Public
+sudo rm -fr ~/Public
+
+####################
+# Kill processes
+####################
+
+killall cfprefsd
+killall Finder
+killall Dock
