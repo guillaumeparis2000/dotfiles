0a2d8e3d3ae9a1d4c3f16bb66e985a0e9d5d0d48
Guillaume PARIS <guillaume.paris@letsbonus.com>
2014-11-21 12:38:57 +0100

    Add homebrew installation file

---
+++ osx/homebrew.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# Check for Homebrew installation
+if test ! $(which brew)
+then
+  echo "Installing Homebrew..."
+
+  # Install homebrew
+  ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+
+fi
+
+# Update homebrew formaulaes
+brew update
+
+# Install some packages
+brew install wget ssh-copy-id coreutils hub
+
+# Tell applications that use xcode-select where the GNU utils are located
+sudo xcode-select --switch /usr/bin
+
+# Install homebrew cask
+brew tap phinze/homebrew-cask
+brew tap caskroom/versions
+brew install brew-cask
