4587ccb85fb354a496c51d66367176e146e8b2b3
Guillaume Paris <guillaume.paris@telefonica.com>
2018-02-02 10:52:40 +0100

    Make shell script executable and remove user part on git config

--- osx/homebrew.sh
+++ osx/homebrew.sh
@@ -6,7 +6,7 @@ then
   echo "Installing Homebrew..."
 
   # Install homebrew
-  ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 fi
 
